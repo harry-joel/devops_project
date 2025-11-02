97017940177faa40589eabb76d0e5d9a4008722e
Harry Joel <harryjoel@karunya.edu.in>
2025-11-02 20:56:54 +0530

    db fix

--- app.py
+++ app.py
@@ -68,6 +68,10 @@ def reset_feedback():
 # --------------------------------------------
 # Run server
 # --------------------------------------------
+# Auto-create tables if they don't exist
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
     if not os.path.exists('feedback.db'):
         with app.app_context():
